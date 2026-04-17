b9cc6157c8de6798062fcb2da06d83f6d81700dd
VisioVortex <mdmainulislamrabby5@gmail.com>
2026-04-17 23:15:52 +0600

    version 0.0.1.2

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-// ()
-// $
-// # &  !
-// %
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
+
+const getCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+}
+
+const playGame = (userChoice) => {
+    console.log("User Choice = ", userChoice);
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
