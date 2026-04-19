1a82963993c128d00db050ac8971b5585b3e74d1
VisioVortex <mdmainulislamrabby5@gmail.com>
2026-04-19 22:53:08 +0600

    Game Version 1.0.0

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 let userScore = 0;
 let compScore = 0;
+
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+
+const userScorePra = document.querySelector("#user-score");
+const compScorePra = document.querySelector("#comp-score");
+
+const drawGame = () => {
+    msg.innerText = "Game was draw! Play again";
+    msg.style.backgroundColor = "#1e272e";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if(userWin){
+        userScore++;
+        userScorePra.innerText = userScore;
+        msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    } else{
+        compScore++;
+        compScorePra.innerText = compScore;
+        msg.innerText = `You Win! ${compChoice} beats your ${userChoice}`;;
+        msg.style.backgroundColor = "red";
+    }
+};
 
 const getCompChoice = () => {
     const options = ["rock", "paper", "scissor"];
     const randIdx = Math.floor(Math.random() * 3);
     return options[randIdx];
-}
+};
 
 const playGame = (userChoice) => {
-    console.log("User Choice = ", userChoice);
-}
+    const compChoice = getCompChoice();
+
+    if(userChoice === compChoice){
+        drawGame();
+    } else{
+        let userWin = true;
+        if(userChoice === "rock"){
+            userWin = compChoice === "paper" ? false : true;
+        } else if(userChoice === "paper"){
+            userWin = compChoice === "scissor" ? false : true;
+        } else{
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
